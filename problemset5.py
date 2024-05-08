686de8058449b0f78cd9b0c80a79dadc5dd039c0
Kalvin Lee <kalvintlee@hotmail.com>
2024-05-08 17:26:00 +0000

    attempt2

--- problemset5.py
+++ problemset5.py
@@ -14,12 +14,18 @@ from bs4 import BeautifulSoup
     # containing all of the python code in the lecture formatted in a way
     # to save it as a python file and run without syntax issues
 def scrape_code(url: str) -> str:
+    # accesses the given url inputted by user
     req_obj = requests.get(url)
+    # parses through the html
     bs = BeautifulSoup(req_obj.text)
+    # locates all python code within the url
     blocks = bs.find_all('code', class_ = 'sourceCode python')
     list = []
     for code in blocks:
+        # splits and separates each line of code within each block of code
         line = code.text.strip().split('\n')
+        # adds each line to the list
         list += line
+    # creates a string of each item in the list, separating each item with a new line
     string = '\n'.join(list)
     return string
