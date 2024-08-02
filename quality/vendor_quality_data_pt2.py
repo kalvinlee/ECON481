d2e15205a904a58e13e37db2f10ba7db82d44208
Kalvin Lee <kalvintlee@hotmail.com>
2024-08-02 18:19:57 +0000

    finale

--- quality/vendor_quality_data_pt2.py
+++ quality/vendor_quality_data_pt2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # For the second part follow the steps 11-14 Vendor Quality Data (based on qty), with the dates as the
-# entire timespan wanted. MAKE SURE PRC IS A COLUMN THAT IS VISIBLE Import the excel file and under the 
+# entire timespan wanted. MAKE SURE Prcpart IS A COLUMN THAT IS VISIBLE! Import the excel file and under the 
 # line file = , replace the name of the new excel file with the old excel file, then run the chunk by 
 # hitting the play on top, and it'll create a new excel file named vendor_quality_data_qty.xlsx which 
 # holds the information for this part of the metric
@@ -10,9 +10,11 @@ def vendor_qual_data_pt2(excel_file):
     df2['Entered On'] = pd.to_datetime(df2['Entered On'], format = '%Y/%m/%d')
     df2['Year-Quarter'] = df2['Entered On'].dt.to_period('Q')
 
-    filtered_df = df2[df2['NCR Type'].str.contains('Manufacturing Defect', case=False, na=False)]
+    filtered_df = df2[df2['NCR Type'].str.contains('Manufacturing Defect', case=False, na=False)].copy()
 
-    result = filtered_df.groupby(['Year-Quarter', 'PRC']).agg({
+    filtered_df['Prcpart_prefix'] = filtered_df['Prcpart'].str[:3]
+
+    result = filtered_df.groupby(['Year-Quarter', 'Prcpart_prefix']).agg({
         'Quantity': 'sum'
     }).reset_index()
 
@@ -20,6 +22,5 @@ def vendor_qual_data_pt2(excel_file):
 
 file2 = "107545-otd_ncrs.xlsx"
 result = vendor_qual_data_pt2(file2)
-print(result)
 output_path = 'vendor_quality_data2.xlsx'
 result.to_excel(output_path, index = False)
